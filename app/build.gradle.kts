dec9c6212822bd7912ef21787842ecf429aff36d
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-01-21 09:01:31 -0800

    [Feature] Bump version to 1.0.3 and add changelogs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "me.zhanghai.android.untracker"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         vectorDrawables { useSupportLibrary = true }
     }
