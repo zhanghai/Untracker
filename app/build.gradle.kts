006dc5d7e193ece519035ee1be01cc709d06af8a
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-01-24 20:15:21 -0800

    Build: Update dependencies and AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,12 +73,11 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.01.00"))
     implementation("androidx.compose.animation:animation-graphics")
     implementation("androidx.compose.material:material-icons-extended")
-    // Using 1.2.0-beta01 for PrimaryTabRow
-    implementation("androidx.compose.material3:material3:1.2.0-beta02")
-    implementation("androidx.compose.material3:material3-window-size-class")
+    // Using 1.2.0-rc01 for PrimaryTabRow
+    implementation("androidx.compose.material3:material3:1.2.0-rc01")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.2.2" apply false
     val kotlinVersion = "1.9.22"
     kotlin("android") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
