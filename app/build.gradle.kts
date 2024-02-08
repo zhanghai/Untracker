d6edcf14430939dd4cdcb967b44cdd57b63e9821
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-02-07 21:57:16 -0800

    Build: Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,11 +73,10 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
-    implementation(platform("androidx.compose:compose-bom:2024.01.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.animation:animation-graphics")
     implementation("androidx.compose.material:material-icons-extended")
-    // Using 1.2.0-rc01 for PrimaryTabRow
-    implementation("androidx.compose.material3:material3:1.2.0-rc01")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -87,7 +86,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("com.github.zhanghai.quickjs-java:quickjs-android:0.9.3")
     implementation("com.mikepenz:aboutlibraries-core:10.9.2")
