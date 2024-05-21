23483a84645178acc40d1306bc862ec6d0239b81
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-05-20 19:38:02 -0700

    Build: Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.4.1" apply false
     val kotlinVersion = "1.9.22"
     kotlin("android") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
