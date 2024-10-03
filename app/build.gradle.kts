86d89a6461ff9046804bd61728f91ad326760527
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-10-02 22:14:01 -0700

    Build: Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
 
-    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
     implementation("androidx.compose.animation:animation-graphics")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
@@ -86,7 +86,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
-    implementation("androidx.navigation:navigation-compose:2.8.1")
+    implementation("androidx.navigation:navigation-compose:2.8.2")
 
     implementation("com.github.zhanghai.quickjs-java:quickjs-android:0.9.3")
     implementation("com.mikepenz:aboutlibraries-core:11.2.3")
