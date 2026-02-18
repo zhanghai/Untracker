4b3fdc937e9d315fce563665d2b940096a8667f4
Hai Zhang <dreaming.in.code.zh@gmail.com>
2026-02-18 15:49:05 -0800

    Build: Update to latest Android Studio project template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,11 +16,20 @@
 
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -31,4 +40,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Untracker"
+
 include(":app")
