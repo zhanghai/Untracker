366958849f7360d02de8b8db074307ac1df14b09
Hai Zhang <zhanghai2048@gmail.com>
2026-05-31 23:21:05 -0700

    Build: Make release signing optional and run full build in CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release").takeIf { it.storeFile != null }
         }
     }
     compileOptions {
